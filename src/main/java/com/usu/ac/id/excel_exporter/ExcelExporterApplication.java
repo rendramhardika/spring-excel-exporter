aaf8bb868c78d2ac7edb4722ca72a3f6a3ac2247
rendramh <rendramahardika@students.usu.ac.id>
2022-02-19 12:11:29 +0700

    spring framework init and add apache poi

---
+++ src/main/java/com/usu/ac/id/excel_exporter/ExcelExporterApplication.java
@@ -0,0 +1,14 @@
+package com.usu.ac.id.excel_exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelExporterApplication.class, args);
+		System.out.println("Excel Exporter Application Started");
+	}
+
+}
---
+++ src/test/java/com/usu/ac/id/excel_exporter/ExcelExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usu.ac.id.excel_exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
